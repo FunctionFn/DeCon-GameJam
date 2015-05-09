abe0778fb3b280d5ae731f629290b4150e050fed
unknown <DFernow@634LAB03.cstcis.cti.depaul.edu>
2015-05-09 14:04:53 -0500

    Added enemy spawning

--- Game/Assets/Scripts/EnemyManager.cs
+++ Game/Assets/Scripts/EnemyManager.cs
@@ -3,13 +3,72 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
 
+    public float spawnTimer;
+    public float spawnInterval;
+    public GameObject player;
+
+    public GameObject zombie;
+    public GameObject fastzombie;
+    public GameObject bigZombie;
+    public GameObject shooter;
+
+    public int spawnRandomizer;
+
+    public int wave;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnInterval)
+        {
+            switch (wave)
+            {
+                case 1:
+                    SpawnEnemy(zombie);
+                    break;
+                case 2:
+                    spawnRandomizer = Random.Range(1, 3);
+                    switch (spawnRandomizer)
+                    {
+                        case 1:
+                            SpawnEnemy(zombie);
+                            break;
+                        case 2:
+                            SpawnEnemy(zombie);
+                            break;
+                        case 3:
+                            SpawnEnemy(fastzombie);
+                            break;
+                    }
+                    break;
+                default:
+
+                    break;
+            }
+            spawnTimer = 0;
+        }
+    
 	}
+
+    void SpawnEnemy(GameObject enemy)
+    {
+        Vector3 spawnPoint = new Vector3(
+            Random.Range(-Player.Inst.clampX, Player.Inst.clampX),
+            Random.Range(-Player.Inst.clampY, Player.Inst.clampY),
+            0f);
+
+        Debug.Log(spawnPoint.ToString());
+        
+        GameObject ego = (GameObject)Instantiate(enemy, spawnPoint, Quaternion.identity);
+        ego.GetComponent<Enemy>().target = player.transform;
+        
+        
+
+
+    }
 }
