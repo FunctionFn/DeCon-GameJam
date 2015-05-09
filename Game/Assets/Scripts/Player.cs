cf6f2233b1a6a6a7eb4630ee13beb190dc9e086c
unknown <DFernow@634LAB03.cstcis.cti.depaul.edu>
2015-05-09 09:01:15 -0500

    First Commit

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        transform.Translate(moveH * speed, moveV * speed, 0);
+    }
+        
+}
